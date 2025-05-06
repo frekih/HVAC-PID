1b1cadf574521ff55c009a82baf040da4dca7b1d
Fredrik Kihlberg <fredrik.kihlberg@norconsult.com>
2025-05-06 13:44:13 +0200

    v1.4

--- spjeldPID.py
+++ spjeldPID.py
@@ -1,18 +1,60 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-class PIDController:
+class PIDController: # Make individual ones, setpoint refers to a 0-value
     def __init__(self, Kp, Ki, Kd, setpoint):
         self.Kp = Kp
         self.Ki = Ki
         self.Kd = Kd
         self.setpoint = setpoint
-        self.previous_error = 5
-        self.integral = 0
+        self.previous_error = 75
+        self.integral = 1250
 
-    def compute(self, process_variable, dt):
+    def compute_safe(self, process_variable_safe, dt):
             # Calculate error
-            error = self.setpoint - process_variable
+            error = self.setpoint - process_variable_safe
+            
+            # Proportional term
+            P_out = self.Kp * error
+            
+            # Integral term
+            self.integral += error * dt
+            I_out = self.Ki * self.integral
+            
+            # Derivative term
+            derivative = (error - self.previous_error) / dt
+            D_out = self.Kd * derivative
+            
+            output = P_out + I_out + D_out
+            
+            self.previous_error = error
+            
+            return output
+
+    def compute_in(self, process_variable_in, dt):
+            # Calculate error
+            error = self.setpoint - process_variable_safe
+            
+            # Proportional term
+            P_out = self.Kp * error
+            
+            # Integral term
+            self.integral += error * dt
+            I_out = self.Ki * self.integral
+            
+            # Derivative term
+            derivative = (error - self.previous_error) / dt
+            D_out = self.Kd * derivative
+            
+            output = P_out + I_out + D_out
+            
+            self.previous_error = error
+            
+            return output
+        
+    def compute_out(self, process_variable_out, dt):
+            # Calculate error
+            error = self.setpoint - process_variable_safe
             
             # Proportional term
             P_out = self.Kp * error
@@ -61,18 +103,18 @@ Nf = int(round(Tf/dt)) + 1
 delay_array = np.zeros(Nf) + t0
 
 # %% Simulation parameters for airflow
-process_variable_out = 850  # Initial extraction airflow
+process_variable_out = 850  # Initial exhaust airflow
 process_values_out = []
 
 process_variable_safe = 200  # Initial safety cabinet airflow
 process_values_safe = []
 
-process_variable_in = process_variable_out + process_variable_safe  # Initial intake airflow
+process_variable_in = process_variable_out + process_variable_safe  # Initial supply airflow
 process_values_in = []
 
-pid_in = PIDController(Kp=0.7, Ki=3.5, Kd=0.85, setpoint=setpoint_in)
-pid_out = PIDController(Kp=0.6, Ki=1.5, Kd=0.75, setpoint=setpoint_out)
-pid_safe = PIDController(Kp=0.7, Ki=2.0, Kd=0.88, setpoint=setpoint_safe)
+pid_in = PIDController(Kp=0.5, Ki=0.5, Kd=1.8, setpoint=setpoint_in)           # PID settings, supply air
+pid_out = PIDController(Kp=0.7, Ki=0.75, Kd=1.5, setpoint=setpoint_out)         # PID settings, exhaust air
+pid_safe = PIDController(Kp=0.5, Ki=0.95, Kd=0.8, setpoint=setpoint_safe)       # PID settings, safety cabinet
 
 # %% Simulate the process
 for t in range(0,N_sim):
@@ -84,15 +126,15 @@ for t in range(0,N_sim):
     # delay_array[1:] = delay_array[0:-1]
     # delay_array[0] = process_variable_safe
 
-    control_output_safe = pid_safe.compute(process_variable_safe, dt)
-    process_variable_safe += control_output_safe * dt - 0.001 * process_variable_safe * dt * t
+    control_output_safe = pid_safe.compute_safe(process_variable_safe, dt)
+    process_variable_safe += control_output_safe * dt - 0.01 * process_variable_safe * dt * t
     process_values_safe.append(process_variable_safe)
     
-    control_output_in = pid_in.compute(process_variable_in, dt)
+    control_output_in = pid_in.compute_in(process_variable_in, dt)
     process_variable_in += control_output_in * dt - 0.01 * process_variable_in * dt * t
     process_values_in.append(process_variable_in + process_variable_safe)
 
-    control_output_out = pid_out.compute(process_variable_out, dt)
+    control_output_out = pid_out.compute_out(process_variable_out, dt)
     process_variable_out += control_output_out * dt - 0.01 * process_variable_out * dt * t
     process_values_out.append(process_variable_out + process_variable_safe)
     
@@ -103,25 +145,25 @@ for t in range(0,N_sim):
     y_safe[t] = process_values_safe[t]
 
 # %% Plot results
-fig, ax1 = plt.subplots()
+fig, ax1 = plt.subplots(figsize = (16, 10))
 ax1.set_xlabel('Time [s]')
-ax1.set_ylabel('Airflow [m3/h]', color='black')
+ax1.set_ylabel('Airflow [m3/h]', color='blue')
 
-plot_1 = ax1.plot(t_array, y_out, label='Process Variable, extract (Airflow [m3/h])')
-plot_1 = ax1.plot(t_array, y_in, label='Process Variable, inflow (Airflow [m3/h])')
-plot_1 = ax1.plot(t_array, y_safe, label='Process Variable, safety cabinet (Airflow [m3/h])')
-ax1.tick_params(axis ='y', labelcolor = 'black')
+plot_1 = ax1.plot(t_array, y_out, color='gold', label='Process Variable, extract (Airflow [m3/h])')
+plot_2 = ax1.plot(t_array, y_in, color='deepskyblue', label='Process Variable, inflow (Airflow [m3/h])')
+plot_3 = ax1.plot(t_array, y_safe, color='salmon', label='Process Variable, safety cabinet (Airflow [m3/h])')
+ax1.tick_params(axis ='y', labelcolor = 'blue')
+
+plot_5 = ax1.hlines(y=setpoint_safe, xmin=t_start, xmax=t_stop, color='m', linestyle='--', label='Setpoint safety cabinet')
+plot_6 = ax1.hlines(y=setpoint_in, xmin=t_start, xmax=t_stop, color='b', linestyle='--', label='Setpoint inflow')
+plot_7 = ax1.hlines(y=setpoint_out, xmin=t_start, xmax=t_stop, color='y', linestyle='--', label='Setpoint extract')
 
 ax2 = ax1.twinx()
 ax2.set_ylabel('Pressure [Pa]', color='green')
-plot_4 = ax2.plot(t_array, p_ref_array, label='Reference pressure')
+plot_4 = ax2.plot(t_array, p_ref_array, color='seagreen', label='Reference pressure')
 ax2.tick_params(axis ='y', labelcolor = 'green')
 
-plot_3 = ax1.plot(setpoint_safe, color='r', linestyle='--', label='Setpoint safety cabinet')
-plot_3 = ax1.plot(setpoint_in, color='b', linestyle='--', label='Setpoint inflow')
-plot_3 = ax1.plot(setpoint_out, color='y', linestyle='--', label='Setpoint extract')
-
+fig.legend()
 plt.title('PID Controller Simulation')
-plt.legend()
 plt.grid()
 plt.show()
